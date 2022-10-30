0a4f5e09881ad687738fcc889ff7cdf55decdcf2
Jason Pollock <jason.pollock@fool.com>
2022-10-29 21:10:48 -0500

    Initial commit

---
+++ rector.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\TypeDeclaration\Rector\Closure\AddClosureReturnTypeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([__DIR__ . '/src']);
+
+    $rectorConfig->skip([
+        '**/vendor/*',
+        '**/node_modules/*',
+        '**/wordpress/*',
+        NewlineAfterStatementRector::class,
+    ]);
+
+    // register a single rule
+    // $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    // $rectorConfig->rule(AddClosureReturnTypeRector::class);
+    // $rectorConfig->rule(AddParamTypeDeclarationRector::class);
+    // $rectorConfig->rule(AddReturnTypeDeclarationRector::class);
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_74,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::TYPE_DECLARATION,
+        SetList::EARLY_RETURN,
+    ]);
+
+    $rectorConfig->importNames();
+    $rectorConfig->parallel();
+};
