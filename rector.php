a71e3f954f776ff5d04010301390e74f1d1a9be9
jason <jason.pollock@fool.com>
2022-11-03 00:47:56 -0500

    Adding stylelint

--- rector.php
+++ rector.php
@@ -2,22 +2,22 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
-use Rector\TypeDeclaration\Rector\Closure\AddClosureReturnTypeRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+// use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+// use Rector\TypeDeclaration\Rector\Closure\AddClosureReturnTypeRector;
+// use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
+// use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
 use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__ . '/src']);
-
     $rectorConfig->skip([
         '**/vendor/*',
         '**/node_modules/*',
         '**/wordpress/*',
+        'themes/freesite-2020-theme/src/Api/Posts.php',
+        'themes/freesite-2020-theme/lib/commerce.php',
         NewlineAfterStatementRector::class,
     ]);
 
